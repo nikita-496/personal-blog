18329e8f016a39265b1c427af2f0339e7b0dc61c
Nikita Shevkunov <shevkunovnikita97@gmail.com>
2022-04-28 12:07:54 +0500

    Creating JWT tokens at authorization

---
+++ server/controller/auth.controller.js
@@ -0,0 +1,80 @@
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const PersonTableExplorer = require("../utils/PersonTableExplorer");
+
+require("dotenv").config();
+
+const handleLogin = async (req, res) => {
+  const { login, password } = req.body[0];
+  if (!login || !password) {
+    return res.status(400).json({ message: "Логин и пароль обязательны" });
+  }
+
+  const authorizer = new PersonTableExplorer();
+  const queryResult = await authorizer.selectLogin();
+  const foundLogin = authCheck();
+
+  if (await validatePwd()) {
+    const accessToken = createAccessJWTs();
+    const refreshToken = createRefreshJWTs();
+    //Сохранение значения refreshToken для пользователя, прошедшего аутентификацию
+    assignTokenToUser();
+    //Хранение токена
+    res.cookie("jwt", refreshToken, {
+      httpOnly: true,
+      maxAge: 24 * 60 * 60 * 1000,
+    });
+    res.json({ accessToken });
+
+    function createAccessJWTs() {
+      const accessToken = jwt.sign(
+        { login: foundLogin },
+        process.env.ACCESS_TOKEN_SECRET,
+        { expiresIn: "30s" }
+      );
+      return accessToken;
+    }
+    function createRefreshJWTs() {
+      const refreshToken = jwt.sign(
+        { login: foundLogin },
+        process.env.REFRESH_TOKEN_SECRET,
+        { expiresIn: "1d" }
+      );
+      return refreshToken;
+    }
+
+    async function assignTokenToUser() {
+      authorizer.loginForSelect = foundLogin;
+      const queryResult = await authorizer.getId();
+      const currentUserId = queryResult.rows[0].id;
+      authorizer.id = currentUserId;
+      authorizer.columnValues = refreshToken;
+      await authorizer.writeToken();
+    }
+  } else {
+    return res
+      .status(401)
+      .json({ message: "Пользователя с такими данными не существует" });
+  }
+
+  function authCheck() {
+    const foundLogin = queryResult.rows.find(
+      (person) => person.login === login
+    );
+    if (!foundLogin) {
+      return res.sendStatus(401); //Unauthorized
+    }
+    return foundLogin.login;
+  }
+
+  //валедировать пароль
+  async function validatePwd() {
+    authorizer.loginForSelect = foundLogin;
+    const foundUserData = await authorizer.selectPwdByLogin();
+    const foundUserRows = foundUserData.rows[0];
+    const match = await bcrypt.compare(password, foundUserRows.password);
+    return match;
+  }
+};
+
+module.exports = { handleLogin };
--- server/index.js
+++ server/index.js
@@ -4,9 +4,11 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 const app = express();
+app.use(express.json());
 
 //routes
-app.use(express.json());
+app.use("/api/v1", require("./routes/auth.routes"));
+
 app.use("/api/v1", require("./routes/user.routes"));
 app.use("/api/v1", require("./routes/post.routes"));
 app.use("/api/v1", require("./routes/register.routes"));
