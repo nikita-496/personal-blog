99584032802b57485eeb7e8b4aa12a21858d6f53
Nikita Shevkunov <shevkunovnikita97@gmail.com>
2022-04-29 13:03:39 +0500

    CORS needs Access-Controll-Allow-Credentials

---
+++ server/config/corsOptions.js
@@ -0,0 +1,15 @@
+const allowedOrigins = require("./allowedOrigins");
+
+const corsOptions = {
+  origin: (origin, callback) => {
+    console.log(allowedOrigins.indexOf(origin));
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  optionSuccessStatus: 200,
+};
+
+module.exports = corsOptions;
