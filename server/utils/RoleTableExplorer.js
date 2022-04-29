d758416e9658907ff4ca3a3e1a2742a89ef0033e
Nikita Shevkunov <shevkunovnikita97@gmail.com>
2022-04-29 07:36:15 +0500

    User roles added

---
+++ server/utils/RoleTableExplorer.js
@@ -0,0 +1,28 @@
+const db = require("../db/db");
+
+class RoleTableExplorer {
+  //Ключ на который ссылается внешний ключ данной таблицы
+  _id = null;
+  _targetUserId = null;
+
+  get id() {
+    return this._id;
+  }
+  set id(val) {
+    return (this._id = val);
+  }
+
+  async getAllRoleValue() {
+    const roleValue = await db.query("SELECT value FROM role");
+    return roleValue;
+  }
+
+  async getRoleValue() {
+    const roleValue = await db.query(
+      `SELECT value FROM role WHERE id = ${this.id}`
+    );
+    return roleValue;
+  }
+}
+
+module.exports = RoleTableExplorer;
---
+++ server/utils/UserRolesTableExplorer.js
@@ -0,0 +1,39 @@
+const db = require("../db/db");
+
+class UserRolesTableExplorer {
+  //Ключ на который ссылается внешний ключ данной таблицы
+  _targetRoleId = null;
+  _targetUserId = null;
+
+  get targetRoleId() {
+    return this._targetRoleId;
+  }
+  set targetRoleId(val) {
+    return (this._targetRoleId = val);
+  }
+
+  get targetUserId() {
+    return this._targetUserId;
+  }
+  set targetUserId(val) {
+    return (this._targetUserId = val);
+  }
+  async createUserRole() {
+    //Каждый зарегистрированный пользователь наделяется правами User,
+    //где 3 - id роли User
+    this.targetRoleId = 3;
+    const newUserRole = await db.query(
+      `INSERT INTO user_roles (user_id, role_id) values(${this.targetUserId}, ${this.targetRoleId})`
+    );
+    return newUserRole.rows[0];
+  }
+
+  async getRoleId() {
+    const roleId = await db.query(
+      `SELECT role_id FROM user_roles WHERE user_id = ${this.targetUserId}`
+    );
+    return roleId;
+  }
+}
+
+module.exports = UserRolesTableExplorer;
