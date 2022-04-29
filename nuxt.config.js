7b19ed67be77d355f433c2f5006f340ad8801dd4
Nikita Shevkunov <shevkunovnikita97@gmail.com>
2022-04-29 11:13:39 +0500

    CORS policy setting

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,49 +1,69 @@
+const dotenv = require("dotenv");
+dotenv.config();
+
+let isDevMode = false;
+if (process.env.NODE_ENV === "development") {
+  isDevMode = true;
+  process.env.PORT = 3000;
+}
+
 export default {
+  srcDir: "client/",
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'personal-blog',
+    title: "personal-blog",
     htmlAttrs: {
-      lang: 'en'
+      lang: "en",
     },
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: "description", name: "description", content: "" },
+      { name: "format-detection", content: "telephone=no" },
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
   },
 
+  /*serverMiddleware: isDevMode
+    ? []
+    : [{ path: "/api", handler: await bootstrap() }],*/
+
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-  ],
+  css: [],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-  ],
+  plugins: [],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-  buildModules: [
-  ],
+  buildModules: [],
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
     // https://go.nuxtjs.dev/axios
-    '@nuxtjs/axios',
+    "@nuxtjs/axios",
+    "@nuxtjs/proxy",
   ],
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: '/',
+    baseURL: isDevMode
+      ? "http://localhost:4000/api"
+      : "https://localhost:3000/api",
+    proxy: true,
+  },
+
+  proxy: {
+    "/api/v1": {
+      target: "http:://localhost:4000/api",
+      pathRewrite: { "^/api/v1": "" },
+      changeOrigin: true,
+    },
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {
-  }
-}
+  build: {},
+};
