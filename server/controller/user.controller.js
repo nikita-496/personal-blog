a28941ec388654fd292b5385244577f392c0ede0
Nikita Shevkunov <shevkunovnikita97@gmail.com>
2022-05-01 16:31:17 +0500

    Correctly deleting and updating blog users in the database

--- server/controller/user.controller.js
+++ server/controller/user.controller.js
@@ -3,7 +3,7 @@ class UserController {
   async createUser(userData) {
     const { name, surname, login, password, email } = userData;
     const newPerson = await db.query(
-      `INSERT INTO person (name, surname, login, password, email) values($1, $2, $3, $4, $5) RETURNING *`,
+      "INSERT INTO person (name, surname, login, password, email) values($1, $2, $3, $4, $5) RETURNING *",
       [name, surname, login, password, email]
     );
     return newPerson.rows[0];
@@ -14,21 +14,27 @@ class UserController {
   }
   async getUser(req, res) {
     const id = req.params.id;
-    const users = await db.query("SELECT * FROM person where id = $1", [id]);
+    const users = await db.query("SELECT * FROM person WHERE id = $1", [id]);
     res.json(users.rows[0]);
   }
   async updateUser(req, res) {
     const { id, name, surname, login, password, email } = req.body[0];
-    console.log(req.body);
     const user = await db.query(
-      "UPDATE person set name = $1, surname = $2, login = $3, password = $4, email = $5 where id = $6 RETURNING *",
+      "UPDATE person set name = $1, surname = $2, login = $3, password = $4, email = $5 WHERE id = $6 RETURNING *",
       [name, surname, login, password, email, id]
     );
     res.json(user.rows[0]);
   }
   async deleteUser(req, res) {
     const id = req.params.id;
-    const user = await db.query("DELETE FROM person where id = $1", [id]);
+    const userRoles = await db.query(
+      "DELETE FROM user_roles WHERE user_id = $1",
+      [id]
+    );
+    const user = await db.query(
+      "DELETE FROM person WHERE id = $1 RETURNING *",
+      [id]
+    );
     res.json(user.rows[0]);
   }
 }
