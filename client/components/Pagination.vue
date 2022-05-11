11eb8a61ca6db52ea9341afdc6060804b3e6150c
Nikita Shevkunov <shevkunovnikita97@gmail.com>
2022-05-11 15:25:22 +0500

    Implementation of pagination logic

---
+++ client/components/Pagination.vue
@@ -0,0 +1,61 @@
+<template>
+  <nav aria-label="Page navigation example" class="flex justify-center">
+    <ul class="flex">
+      <li>
+        <button
+          class="py-3 px-3 mr-4 text-main-dark text-lg bg-blueGray-100 rounded-md border border-gray-100 hover:bg-blueGray-400 hover:text-main-light transition-all duration-300 ease-linear cursor-pointer"
+          :disabled="currentPage === 1"
+          :class="currentPage === 1 ? 'disabled' : ''"
+          @click="sendNumberPage(currentPage - 1)"
+        >
+          Назад
+        </button>
+      </li>
+      <li v-for="numberPage in totalPages" :key="numberPage">
+        <button
+          class="py-3 px-4 text-main-dark text-lg text-dark bg-blueGray-100 border border-gray-100 hover:text-main-light transition-all duration-300 ease-linear cursor-pointer"
+          :class="
+            currentPage === numberPage ? 'active-page' : 'hover:bg-blueGray-400'
+          "
+          @click="sendNumberPage(numberPage)"
+        >
+          {{ numberPage }}
+        </button>
+      </li>
+      <li>
+        <button
+          class="py-3 px-4 ml-4 text-main-dark text-lg bg-blueGray-100 rounded-md border border-gray-100 hover:bg-blueGray-400 hover:text-main-light transition-all duration-300 ease-linear cursor-pointer"
+          :disabled="currentPage === totalPages"
+          :class="currentPage === totalPages ? 'disabled' : ''"
+          @click="sendNumberPage(currentPage + 1)"
+        >
+          Вперед
+        </button>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+  props: {
+    currentPage: { type: Number, required: true, default: 1 },
+    totalPages: { type: Number, required: true },
+  },
+  methods: {
+    sendNumberPage(clickedPage) {
+      console.log(clickedPage);
+      this.$emit("updatePage", clickedPage);
+    },
+  },
+};
+</script>
+
+<style>
+.active-page {
+  @apply bg-blue-600  hover:opacity-75 text-main-light;
+}
+.disabled {
+  @apply bg-gray-400 hover:text-main-dark hover:bg-gray-400 cursor-not-allowed;
+}
+</style>
--- client/pages/article/all.vue
+++ client/pages/article/all.vue
@@ -1,43 +1,50 @@
 <template>
-  <ul
-    class="grid grid-rows-12 sm:grid-rows-6 lg:grid-rows-4 grid-flow-col gap-x-6 gap-y-6"
-  >
-    <li class="w-72 rounded-md" v-for="article in articles" :key="article.id">
-      <div>
-        <img
-          class="rounded-md h-48 w-full"
-          src="../../assets/img/mountains.jpg"
-          alt="Горы"
-        />
-        <div class="text-xl font-bold pt-4" v-html="article.title"></div>
-        <div class="py-4 text-main-dark">
-          <span class="font-bold">
-            Аннотация.
-            <span class="font-normal">
-              Аннотация - краткое точное изложение содержания статьи, включающее
-              основные фактические сведения и выводы описываемой работы. Цель
-              аннотации – дать читателю представление о содержании статьи без
-              ознакомления с полным текстом.
+  <div>
+    <ul
+      class="grid grid-rows-12 sm:grid-rows-6 lg:grid-rows-4 grid-flow-col gap-x-6 gap-y-6 mb-20"
+    >
+      <li class="w-72 rounded-md" v-for="article in articles" :key="article.id">
+        <div>
+          <img
+            class="rounded-md h-48 w-full"
+            src="../../assets/img/mountains.jpg"
+            alt="Горы"
+          />
+          <div class="text-xl font-bold pt-4" v-html="article.title"></div>
+          <div class="py-4 text-main-dark">
+            <span class="font-bold">
+              Аннотация.
+              <span class="font-normal">
+                Аннотация - краткое точное изложение содержания статьи,
+                включающее основные фактические сведения и выводы описываемой
+                работы. Цель аннотации – дать читателю представление о
+                содержании статьи без ознакомления с полным текстом.
+              </span>
             </span>
-          </span>
-        </div>
-        <div class="pb-4 font-raleway font-medium text-blueGray-400">
-          <span
-            class="after:content-['●'] after:px-4 after:text-base after:text-gray-300"
+          </div>
+          <div class="pb-4 font-raleway font-medium text-blueGray-400">
+            <span
+              class="after:content-['●'] after:px-4 after:text-base after:text-gray-300"
+            >
+              20 июл, 2021</span
+            >
+            <span>JavaScript</span>
+            <span>Функциональное программирование</span>
+          </div>
+          <nuxt-link
+            class="text-blue-800 font-medium hover:opacity-75"
+            :to="{ path: `${article.id}`, params: { articleId: article.id } }"
+            >Читать</nuxt-link
           >
-            20 июл, 2021</span
-          >
-          <span>JavaScript</span>
-          <span>Функциональное программирование</span>
         </div>
-        <nuxt-link
-          class="text-blue-800 font-medium hover:opacity-75"
-          :to="{ path: `${article.id}`, params: { articleId: article.id } }"
-          >Читать</nuxt-link
-        >
-      </div>
-    </li>
-  </ul>
+      </li>
+    </ul>
+    <pagination
+      :totalPages="Math.floor(pagination.totalPages / 12)"
+      :currentPage="pagination.currentPage"
+      @updatePage="changePage"
+    />
+  </div>
 </template>
 
 <script>
@@ -46,19 +53,36 @@ export default {
   data() {
     return {
       articles: [],
+      pagination: {
+        totalPosts: null,
+        currentPage: null,
+      },
     };
   },
   async asyncData() {
     let articles;
+    let pagination = {};
     try {
-      await getJSON(`${API.post}?page=1&limit=12`).then(
-        (res) => (articles = res.data.results)
-      );
-
-      return { articles };
+      await getJSON(`${API.post}?page=1&limit=12`).then((res) => {
+        articles = res.data.results;
+        pagination.totalPages = res.data.total;
+        pagination.currentPage = res.data.next.page - 1;
+      });
+      return { articles, pagination };
     } catch (err) {
       console.error(err);
     }
+  },
+  methods: {
+    async changePage(pageForActivation) {
+      await getJSON(`${API.post}?page=${pageForActivation}&limit=12`).then(
+        (res) => {
+          this.articles = res.data.results;
+          this.pagination.totalPages = res.data.total;
+          this.pagination.currentPage = res.data.next.page - 1;
+        }
+      );
+    },
   },
 };
 </script>
