6f6ea04289b4c14ddf904a2b018643b0bb563098
Nikita Shevkunov <shevkunovnikita97@gmail.com>
2022-05-11 07:09:23 +0500

    Fix partial issue of articles

--- server/middleware/paginatedResults.js
+++ server/middleware/paginatedResults.js
@@ -6,6 +6,7 @@ function paginatedResults(model) {
       results.results = model;
       res.paginatedResults = results;
       next();
+      return;
     }
     const page = parseInt(req.query.page);
     const limit = parseInt(req.query.limit);
@@ -13,7 +14,6 @@ function paginatedResults(model) {
     const startIndex = (page - 1) * limit;
     const endIndex = page * limit;
 
-    console.log(model);
     if (endIndex < model.length) {
       results.next = {
         page: page + 1,
@@ -30,7 +30,6 @@ function paginatedResults(model) {
 
     results.results = model.slice(startIndex, endIndex);
     res.paginatedResults = results;
-    console.log((res.paginatedResults = results));
     next();
   };
 }
