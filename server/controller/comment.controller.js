d307c1c52f3982acfb977500dba28c46cd9a65b6
Nikita Shevkunov <shevkunovnikita97@gmail.com>
2022-05-23 07:28:03 +0500

    Assigning an author to a comment

--- server/controller/comment.controller.js
+++ server/controller/comment.controller.js
@@ -2,7 +2,7 @@ const CommentTableExplorer = require("../utils/db_interection/CommentTableExplor
 
 const handleComment = async (req, res) => {
   if (req.method !== "GET" && req.method !== "DELETE") {
-    var { content, post_id, forum_id, id } = req.body[0];
+    var { content, post_id, forum_id, user_id, id } = req.body[0];
   }
 
   const method = req.method;
@@ -13,16 +13,23 @@ const handleComment = async (req, res) => {
   handler.content = content;
   handler.postId = post_id;
   handler.forumId = forum_id;
+  handler.userId = user_id;
 
   method === "POST" ? await create() 
   : method === "PUT" ? await update()
   : method === "DELETE" ? await remove()
-  : (result = res.json(await handler.getComments()));
+  : await get();
 
   return result;
 
   async function create() {
     result = res.json(await handler.createComment());
+  }
+  async function get() {
+    if (req.params.id) {
+      handler.id = req.params.id;
+    }  
+    result = res.json(await handler.getComment());
   }
   async function update() {
     handler.id = id;
--- server/utils/db_interection/CommentTableExplorer.js
+++ server/utils/db_interection/CommentTableExplorer.js
@@ -4,6 +4,7 @@ class CommentTableExplorer {
   _content = null;
   _post_id = null;
   _forum_id = null;
+  _user_id = null;
   _id = null;
 
   get content() {
@@ -27,6 +28,13 @@ class CommentTableExplorer {
     return (this._forum_id = val);
   }
 
+  get userId() {
+    return this._user_id;
+  }
+  set userId(val) {
+    return (this._user_id = val);
+  }
+
   get id() {
     return this._id;
   }
@@ -35,13 +43,12 @@ class CommentTableExplorer {
   }
 
   async createComment() {
-    console.log(this.content, this.postId)
     const date = await db.query("SELECT TO_CHAR(NOW() :: DATE, 'Mon dd, yyyy')");
     const time = await db.query("SELECT LOCALTIMESTAMP(0) :: TIME");
 
     const newComment = await db.query(
-      "INSERT INTO comment (content, post_id, forum_id, publication_date, publication_time) values($1, $2, $3, $4, $5) RETURNING *",
-      [this.content, this.postId, this.forumId,  date.rows[0].to_char, time.rows[0].localtimestamp]
+      "INSERT INTO comment (content, post_id, forum_id, user_id, publication_date, publication_time) values($1, $2, $3, $4, $5, $6) RETURNING *",
+      [this.content, this.postId, this.forumId, this.userId,date.rows[0].to_char, time.rows[0].localtimestamp]
     );
     return newComment.rows[0];
   }
@@ -49,6 +56,11 @@ class CommentTableExplorer {
   async getComments() {
     const comments = await db.query("SELECT * FROM comment")
     return comments.rows
+  }
+
+  async getComment() {
+    const comment = await db.query(`SELECT * FROM comment WHERE id = $1`, [this.id])
+    return comment.rows
   }
 
   async updateComments() {
